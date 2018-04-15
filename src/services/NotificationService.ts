8d5dbda475ebea6bfcb7590851197a7e6532363a
Mohsen Esmaeli <m-esm@hotmail.com>
2018-04-15 10:38:01 +0430

    progress

--- src/services/NotificationService.ts
+++ src/services/NotificationService.ts
@@ -1,24 +1,39 @@
-import { ServerServiceInterface, EmailService, Server, EmailModel } from "serendip";
+import { ServerServiceInterface, EmailService, Server, EmailModel, SmsIrService } from "serendip";
 
 export class NotificationService implements ServerServiceInterface {
 
-    static dependencies = ["DbService", "SmsService", "EmailService", "FaxService"];
+    static dependencies = ["DbService", "SmsIrService", "EmailService", "FaxService"];
 
     _emailService: EmailService;
+    _smsIrService: SmsIrService;
+
+    constructor() {
+
+        this._emailService = Server.services["EmailService"];
+        this._smsIrService = Server.services["SmsIrService"];
+
+    }
 
     async start() {
 
-        this._emailService = Server.services["EmailService"];
+        // try {
+        //     var credit = await this._smsIrService.credit();
+        //     console.log(`remaining credit in sms.ir service : ${credit}`);
+        // } catch (e) {
+        //     console.log(e);
+
+        // }
+        // this._emailService = Server.services["EmailService"];
 
         // this._emailService.send({
         //     from: 'dev@serendip.agency',
-        //     to: 'test@serendip.agency',
+        //     to: 'nowroozi.vahid@gmail.com',
         //     subject: 'Verify your email address on serendip.agency',
         //     template: {
-        //         data : {
-        //             name : 'mohsen'
+        //         data: {
+        //             name: 'mohsen'
         //         },
-        //         name : 'verify_email'
+        //         name: 'verify_email'
         //     }
         // }).then((msg) => {
 
